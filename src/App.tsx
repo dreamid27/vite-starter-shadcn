64953108c7f688c259b9cab51303dcd92eddb793
Muhammad Al Faris <muh.alfaris@gmail.com>
2025-06-07 11:43:07 +0700

    chore: change default theme from dark to light mode

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <TooltipProvider>
         <Toaster />
         <BrowserRouter>

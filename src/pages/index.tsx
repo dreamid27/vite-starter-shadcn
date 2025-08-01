48e51adaa919ddf60a202b2183326009fef3f863
Muhammad Al Faris <muh.alfaris@gmail.com>
2025-08-01 19:34:33 +0700

    fix: init index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { HeroSection } from "@/components/blocks/hero-section-1";
+import { HeroSection } from '@/components/blocks/hero-section-1';
 
 const Index = () => {
-  return (
-    <>
-      <HeroSection />
-    </>
-  );
+  return <></>;
 };
 
 export default Index;
